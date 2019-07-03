be55d8148ead91555c72a4a8710a6b485ec848d4
William del Rosario <williamdelrosario@yahoo.com>
2019-07-03 23:33:13 +0200

    A generic Dispatch function

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -8,13 +8,20 @@ interface Todo
     title: string,
     completed: boolean
 }
+
+interface FetchTodosAction
+{
+    type: ActionTypes.fetchTodos;
+    payload: Todo[];
+}
+
 const url = 'https://jsonplaceholder.typicode.com/todos';
 
 export const fetchTodos = () => {
     return async (dispatch: Dispatch) => {
         const response = await axios.get<Todo[]>(url);
 
-        dispatch({
+        dispatch<FetchTodosAction>({
             type: ActionTypes.fetchTodos,
             payload: response.data,
         });
