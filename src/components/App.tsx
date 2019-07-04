3c17b253a736e5446cd57c74e81f0af47cb5b657
William del Rosario <williamdelrosario@yahoo.com>
2019-07-04 07:38:21 +0200

    Rendering a list

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -11,8 +11,28 @@ interface AppProps
 
 class _App extends React.Component<AppProps>
 {
+    // componentDidMount(): void {
+    //     this.props.fetchTodos();
+    // }
+
+    onButtonClick = (): void => {
+        this.props.fetchTodos();
+    };
+
+    renderList(): JSX.Element[] {
+        return this.props.todos.map((todo: Todo) => {
+           return <div key={todo.id}>{todo.title}</div>
+        });
+    }
+
     render() {
-        return <div>Hi there</div>;
+        // console.log(this.props.todos);
+        return (
+            <div>
+                <button onClick={this.onButtonClick}>Fetch</button>
+                {this.renderList()}
+            </div>
+        );
     }
 }
 
